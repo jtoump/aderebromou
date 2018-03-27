1a7de959e59d495ec7e2a98e92d76beaab12a68d
Ioannis Toumpalidis <t_ejohn@hotmail.com>
2018-03-27 18:12:18 +0100

    test

--- index.js
+++ index.js
@@ -154,11 +154,16 @@ function init(){
                     // vertex.y = -obj.data[i].killer_position_y;
                     // vertex.z = obj.data[i].killer_placement*10;
                     //console.log(vertex.z);
-                    particlePositions.push(obj.data[i].killer_position_x*0.01);
+                          
+                          
+                          particlePositions.push(i);
+                          particlePositions.push(1);
+                          particlePostiions.push(1);
+//                     particlePositions.push(obj.data[i].killer_position_x*0.01);
                     
-                    particlePositions.push(-obj.data[i].killer_position_y*0.01);
+//                     particlePositions.push(-obj.data[i].killer_position_y*0.01);
                     
-                    particlePositions.push(obj.data[i].heihgtxy*0.001);
+//                     particlePositions.push(obj.data[i].heihgtxy*0.01);
                     //linegeometry.vertices.push(new THREE.Vector3(obj.data[i].killer_position_x,-obj.data[i].killer_position_y,obj.data[i].killer_placement*10));
                     
                     //geometry.colors.push(new THREE.Color(obj.data[i].ppl_killed*0.1, obj.data[i].killer_placement*0.01, 0));
